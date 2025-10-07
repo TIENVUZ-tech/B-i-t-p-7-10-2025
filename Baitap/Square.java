6e43270c6f3da9b5d5c7674be1d264e2f405052b
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-07 11:42:08 +0700

    Create Square.java

---
+++ Baitap/Square.java
@@ -0,0 +1,11 @@
+package Baitap;
+public class Square extends Shape {
+    public Square(Point centrePoint) {
+        super(centrePoint);
+    } 
+
+    @Override
+    public String toString() {
+        return "Hinh Vuong[" + centrePoint + "]";
+    }
+}
