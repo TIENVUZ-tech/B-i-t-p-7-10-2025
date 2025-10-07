0f280b71c40c990c3d40ae305aa9325c8bd67ec9
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-07 11:42:02 +0700

    Create Rectangle.java

---
+++ Baitap/Rectangle.java
@@ -0,0 +1,13 @@
+package Baitap;
+
+public class Rectangle extends Shape {
+    
+    public Rectangle(Point centrePoint) {
+        super(centrePoint);
+    } 
+
+    @Override
+    public String toString() {
+        return "Hinh chu nhat[" + centrePoint + "]";
+    }
+}
