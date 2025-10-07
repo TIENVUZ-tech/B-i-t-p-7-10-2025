259e5943758e5fa88e0431e124445f18e2c43142
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-07 10:32:42 +0700

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square extends Shape {
+    public Square(Point centrePoint) {
+        super(centrePoint);
+    } 
+
+    @Override
+    public String toString() {
+        return "Hinh Vuong" + centrePoint + "]";
+    }
+}
