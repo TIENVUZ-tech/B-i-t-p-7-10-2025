c2ba6dc023ad5fd876865f08cd1dd7748985b0ca
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-07 10:32:28 +0700

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,11 @@
+public class Circle extends Shape {
+    public Circle(Point centrePoint) {
+        super(centrePoint);
+    } 
+
+    @Override
+    public String toString() {
+        return "Hinh chu nhat[" + centrePoint + "]";
+    }
+}
+
