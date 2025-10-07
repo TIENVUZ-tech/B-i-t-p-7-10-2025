f4e717e696261271fccfedd768f72071e6714604
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-07 10:31:01 +0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,19 @@
+public abstract class Shape {
+    protected Point centrePoint;
+    private static int counter = 0;
+
+    public Shape(Point centrePoint) {
+        this.centrePoint = centrePoint;
+        this.counter++;
+    }
+
+    public static int getShapeCount() {
+        return counter;
+    }
+    public Point getCentrePoint() {
+        return centrePoint;
+    }
+    public String toString() {
+        return " " + centrePoint;
+    }
+}
